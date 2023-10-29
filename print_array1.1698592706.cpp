146b302720f7e7494af018412f2244cd745c0fd1
jyPark1105 <128760195+jyPark1105@users.noreply.github.com>
2023-10-29 15:22:07 +0900

    print_array1
    1. You can put the numbers to array whatever those are.
    2. The insertion sort will make the array sorted.
    3. print the odds and the evens in order.

---
+++ print_array1.1698592706.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+
+int define_Size(void)											// Set the input size
+{
+	int size;
+	printf("Enter the array size, smaller than '256' : ");		// Start the program
+	scanf_s("%d", &size);										// Set the Array SIZE
+	return size;
+}
+
+void Insertion_Sort(int* arr, int SIZE)							
+{
+	int key = 0;
+	int i, j;
+	for (j = 1; j < SIZE; j++)			// 1st index's key value is "already sorted" first (from 2nd index to Last index)
+	{
+		key = arr[j];
+		i = j - 1;
+		while (i >= 0 && arr[i] > key)	// Insertion Sort : stable --> thus, not equal
+		{
+			arr[i + 1] = arr[i];
+			i--;
+		}
+		arr[i+1] = key;
+	}
+	printf("Sorted array1 : {");
+	for (i = 0; i < SIZE - 1; i++)
+	{
+		printf("%d, ", arr[i]);
+	}
+	printf("%d}\n\n", arr[SIZE - 1]);
+}
+
+void even_num(int *arr, int SIZE) // int *p = &arr;
+{
+	int i;
+	for(i = 0; i < SIZE; i++)
+	{
+		if (arr[i] % 2 == 0)
+			printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void odd_num(int *arr, int SIZE)
+{
+	int j;
+	for (j = 0; j < SIZE; j++)
+	{
+		if (arr[j] % 2 == 1)
+			printf("%d ", arr[j]);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	int arr_val, i;							// Value of Array -> arr_num
+	int arr_size = define_Size();
+	int array1[256] = {};
+	for (i = 0; i < arr_size; i++)
+	{
+		printf("Array[%d] : ", i);
+		scanf_s("%d", &arr_val);
+		array1[i] = arr_val;
+	}
+	printf("Original Array : {");
+	for (i = 0; i < arr_size - 1; i++)
+	{
+		printf("%d,", array1[i]);
+	}
+	printf("%d}\n\n", array1[i]);
+	Insertion_Sort(array1, arr_size);
+	printf("odd : ");
+	odd_num(array1, arr_size);
+	printf("even : ");
+	even_num(array1, arr_size);
+	return 0;
+}
